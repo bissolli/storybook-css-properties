6e1c4f648760e41582b2a71067f69f3e79b736d4
Gustavo Bissolli <gustavo.bissolli@gmail.com>
2022-04-22 12:05:32 +0400

    docs: update readme

--- src/preset/manager.ts
+++ src/preset/manager.ts
@@ -8,7 +8,7 @@ addons.register(ADDON_ID, () => {
   // Register the panel
   addons.add(PANEL_ID, {
     type: types.PANEL,
-    title: "Color System",
+    title: "CSS Properties",
     match: ({ viewMode }) => viewMode === "story",
     render: Panel,
   });
